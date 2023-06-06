068467771cd1f9c3a6a78a01c1f85b93e8455d5f
Harsha <shreeharsha.a@improve10x.com>
2023-06-06 15:48:57 +0530

    Created a model classes

---
+++ app/src/main/java/com/example/quizapp/model/Quiz.java
@@ -0,0 +1,36 @@
+package com.example.quizapp.model;
+
+import java.util.ArrayList;
+
+public class Quiz {
+
+    private String id;
+
+    private Module module;
+
+    private ArrayList<Questions> questions;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Module getModule() {
+        return module;
+    }
+
+    public void setModule(Module module) {
+        this.module = module;
+    }
+
+    public ArrayList<Questions> getQuestions() {
+        return questions;
+    }
+
+    public void setQuestions(ArrayList<Questions> questions) {
+        this.questions = questions;
+    }
+}
